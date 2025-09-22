0825373d3991504de122f12a9999b3bb6cd3b77d
William <126926699+GitXpresso@users.noreply.github.com>
2025-09-22 09:08:04 -0400

    Update installwaterfox.sh

--- installwaterfox.sh
+++ installwaterfox.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 waterfox_binary_install_deb(){
-libx11-xcb1
 binary=("libasound2-dev" "libgtk-3-0" "libx11-xcb1")
 dpkg -l | grep -qw ${binary_setup}
 for binary_setup in "${binary[@]}"; do
@@ -26,9 +25,24 @@ fi
 # Bash script install waterfox on Arch Linux, Fedora Linux, Debian and Debian based distros
 waterfox_latest_version="6.6.3"
 if [ -d /etc/apt ]; then
+  wget -P ~/ --show-progress -q https://github.com/GitXpresso/LinuxPKG/releases/download/ITB/waterfox_deb_dir.tar
   cd ~/
   wget -P ~/ -q --show-progress https://github.com/gitxpresso/releases/download/Waterfox/waterfox_dir.tar
   tar -xvf  ~/waterfox_dir.tar -C ~/
   wget -P ~/ -q --show-progress https://cdn1.waterfox.net/waterfox/releases/$waterfox_latest_version/Linux_x86_64/waterfox-$waterfox_latest_version.tar.bz2
-  tar -xvf ~/*.bz2 -C ~/waterfox
+  tar -xvf ~/*.bz2 -C ~/waterfox-6.6.3/usr/lib/
+  waterfox_binary_install_deb
+  if [ ! $(dpkg -l | grep -qw dpkg-dev) ]; then
+    echo "dpkg-deb not installed, installing..."
+    sudo apt install dpkg-deb -y
+    clear
+  fi
+  if [ ! $(dpkg -l | grep -qw build-essential) ]; then
+      echo "build essential not installed, installing..."
+      sudo apt install -y build-essential
+      clear
+  fi
+  dpkg-deb -b ~/waterfox-6.6.3
+  fi
   # Not finished
+
